8b30796738676b2f46f32ff07a80cd43383258fb
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-13 15:53:51 -0300

    aula14

---
+++ aula14/aula14.py
@@ -0,0 +1,18 @@
+'''
+str ou string: são caracteres inseridos entre aspas num comando .
+como print('') abaixo
+o interpretador le esses caracteres como texto
+'''
+print('essa é uma string (str)')
+
+# para usar aspas numa string deve se usar um tipo diferente da  que foi usada para abrir e fechar o comando. ex abaixo:
+print('essa é uma string com "aspas"')
+
+# outra forma de usar é utilizando um caractere de escape (\) antes das aspas que deseja inserir na string. ex abaixo:
+print("essa é uma string com \"aspas\" usando caractere de escape (\)")
+
+# /n insere uma quebra de linha na string
+print('quebrando \n a linha ')
+
+# para inserir um comando numa string e fazer com que seja lido como string e não interpretado adicione r antes das aspas de inicio ex abaixo:
+print(r'inserindo \n como string')
