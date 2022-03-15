5addc8d781618f2b446e2b0cf816aa7e8dde5e06
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-15 20:04:07 -0300

    aula18

---
+++ aula18/variaveis.py
@@ -0,0 +1,27 @@
+'''
+pode:
+iniciar com letra, conter numeros(no no inicio), separar com ",_" letras minusculas
+'''
+
+nome = 'Gustavo'  #str
+idade = 17  #int
+altura = 1.75  #float
+e_maior = idade > 18  #bool
+peso = 52  #int
+imc = 16.979591837  #float
+
+
+
+
+print('nome:',nome)
+
+print('idade:',idade)
+
+print('altura:',altura)
+
+print('e_maior:',e_maior)
+
+print(idade * altura)  #tbm é possivel usar operadores com variaveis. as regras são as mesmas
+
+print(nome, type(nome))  #tbm pode-se ver o tipo de valor da variavel usando type()
+
