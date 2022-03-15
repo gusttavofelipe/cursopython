031868f2c0b875ee03c957887a61117f398a6bce
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-15 20:17:05 -0300

    exercicio

---
+++ aula18/exercicico.py
@@ -0,0 +1,10 @@
+  # exibir o valor do meu IMC usando as variaveis declaradas abaixo
+
+nome = 'Gustavo'  #str
+idade = 17  #int
+altura = 1.75  #float
+e_maior = idade > 18  #bool
+peso = 52  #int
+imc = 16.979591837  #float
+
+print(nome,'tem',idade,'anos de idade','tem',altura ,'de altura, pesa',peso,'kg','e seu IMC é',imc,'kg')
