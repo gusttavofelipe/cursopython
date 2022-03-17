618edbff1a8783da7a2714d0ebc5d166771d5795
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-17 20:59:24 -0300

    aula21-desafio

---
+++ aula21-desafio/desafio.py
@@ -0,0 +1,11 @@
+nome = 'Gustavo'
+idade = 17
+altura = 1.75
+peso = 52
+imc = peso / altura ** 2
+ano_atual = 2022
+ano_de_nascimento = ano_atual - idade
+
+print(f'{nome} tem {idade} anos, {altura:.1f} de altura e pesa {peso}kg')
+print(f'o IMC de {nome} é {imc:.2f}')
+print(f'{nome} nasceu em {ano_de_nascimento} ')
