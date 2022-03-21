10ca76a12018bac02894644789517b627ba8a10b
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-20 21:16:16 -0300

    aula25

---
+++ aula25/operadores-relacionais.py
@@ -0,0 +1,63 @@
+'''operadores relacionais
+== igual?
+ > maior que
+ >= maior ou igual a
+  < menor que
+  <= menor que ou igual a
+   != diferente
+'''
+
+
+''' == operador de igualdade, é interpretado como a pergunta: "tal coisa" é igual a "tal coisa"? e retorna  valor true ou false'''
+# pode-se usar outros tipos de valores além de int e float
+num1 = 1
+num2 = 1
+print(num1 == num2)
+
+# é possivel atribuir esse operador a uma varivel
+# pode-se usar outros tipos de valores além de int e float
+expressao = num1 == num2
+print(expressao)
+
+''' > operador de maioridade (maior que), é interpretado como a pergunta: "tal coisa" é maior que "tal coisa"? e retorna  valor true ou false'''
+# pode-se usar outros tipos de valores além de int e float
+num1 = 2
+num2 = 1
+expressao = num1 > num2
+print(expressao)
+
+''' >= operador de maioridade e igualdade: "tal coisa" é maior ou igual a "tal coisa"? e retorna  valor true ou false'''
+# pode-se usar outros tipos de valores além de int e float
+num1 = 1
+num2 = 1
+expressao = num1 >= num2
+print(expressao)
+
+''' < operador de minoridade, menor que: "tal coisa" é menor que "tal coisa"?'''
+# pode-se usar outros tipos de valores além de int e float
+num1 = 1
+num2 = 2
+expressao = num1 < num2
+print(expressao)
+
+''' <= operador de minoridade e igualdade: "tal coisa" é menor ou igual a "tal coisa"?'''
+# pode-se usar outros tipos de valores além de int e float
+num1 = 2
+num2 = 2
+expressao = num1 <= num2
+print(expressao)
+
+''' != operador de diferença: "tal coisa" é diferente de "tal coisa"?'''
+#pode-se usar outros tipos de valores além de int e float
+num1 = 'Gustavo'
+num2 = 'Gustavo'
+expressao = num1 != num2
+print(expressao)
+
+
+
+
+
+
+
+
