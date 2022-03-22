a028ece157b54a5cda9b88adb0e6592dcd5ec352
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-21 21:15:00 -0300

    exemplo aula26

---
+++ aula26/exemplo_de_usuabilidade.py
@@ -0,0 +1,8 @@
+usuario = input('Usuário: ')
+senha = input('Senha: ')
+
+usuario_bd = 'gustavo'
+senha_bd = "12345"
+
+if usuario == usuario_bd and senha == senha_bd:
+    print(f'Usuário {usuario_bd} logado')
