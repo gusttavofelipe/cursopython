822e4a5212d3af9dba79711ca8d1c8abcf88c03d
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-03-24 19:31:49 -0300

    aula29

---
+++ aula29/pass-ellipses.py
@@ -0,0 +1,9 @@
+
+
+valor = True
+
+if valor:
+    ... # caso queira deixar um espaço sem nada ou para usar depois, use 'pass', assim o interpretador não retorna código de erro
+        # em alguns casos tbm pode-se usar ellipses (...) para fazer essa função
+else:
+    print('Bye')
