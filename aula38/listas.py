99f9f8fea162ab5ede0f3ed3c1b0c37d1c4c45a6
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-04-04 20:02:46 -0300

    mais anotações

--- aula38/listas.py
+++ aula38/listas.py
@@ -7,10 +7,15 @@ lista = [132434, 'Olá', True, False, 45.545]
 # -        -5     -4     -3    -2      -1
 lista[-3] = 'Outro valor' #é possível mudar o valor da lista usando seu indice
 print(lista[-3])
-print(lista[-3:-1])  #tbm é possível usar o fatiamento para mostrar mais de valor da lista
+print(lista[-3::-2])  #tbm é possível usar o fatiamento para mostrar mais de valor da lista
 '''
 nesse caso:
 0 é o valor incial que será exibido
 3 é o ultimo valor que será exibido
 "lembrando que os valores são exibidos apenas até um número antes do indicado"
+pode-se tbm exibir os valores "pulando valores":
+print(lista[-3::-1])
+onde: -3 é o valor incial que será exibido
+:: (se) nada estiver entre os dois pontos significa que ele seguirá a sequencia linear completa
+-2 é de quanto em quanto pular
 '''
