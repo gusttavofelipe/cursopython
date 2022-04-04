75f71236d8ca8accaecbf3dbeee918724e5c7ca2
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-04-03 21:48:05 -0300

    aula38 parte1

---
+++ aula38/listas.py
@@ -0,0 +1,16 @@
+#Listas: podem conter mais de um valor, suporta diferentes tipos de valores
+#Esses valores são colocados entre chaves, e para separar um valor de outro, usa-se vírgula
+#Para acessar um valor individual usa-se indices, o diferencial é q cada indice pode conter um valor do tamanho desejado
+
+#           0      1      2     3      4
+lista = [132434, 'Olá', True, False, 45.545]
+# -        -5     -4     -3    -2      -1
+lista[-3] = 'Outro valor' #é possível mudar o valor da lista usando seu indice
+print(lista[-3])
+print(lista[-3:-1])  #tbm é possível usar o fatiamento para mostrar mais de valor da lista
+'''
+nesse caso:
+0 é o valor incial que será exibido
+3 é o ultimo valor que será exibido
+"lembrando que os valores são exibidos apenas até um número antes do indicado"
+'''
