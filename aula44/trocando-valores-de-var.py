e77bd65f95a1a1ca4c3b65a6b50453b6c78a503d
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-04-28 14:31:54 -0300

    trocando valores de variaveis

---
+++ aula44/trocando-valores-de-var.py
@@ -0,0 +1,15 @@
+
+'''Para trocar o valor de variaveis: basta, primeiro indicar quais variaveis IRÃO receber o valor de outras variaveis, e depois indicar
+RESPECTIVAMENTE quais variaveis SERÃO aquelas que irão se TORNAR o valor das variaveis indicadas primariamente.'''
+
+g = 'o'
+u = 'v'
+s = 'a'
+t = 't'
+a = 's'
+v = 'u'
+o = 'g'
+
+g, u, s, t, a, v, o = o, v, a, t, s, u, g
+
+print(g, u, s, t, a, v, o)
