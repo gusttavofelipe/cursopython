e241b29449b4f2929922edf2fa85a3fe930b209b
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-05-10 20:29:49 -0300

    modificação

--- aula52/def2.py
+++ aula52/def2.py
@@ -1,48 +1,48 @@
 
 
-# def valor(funcao):
-#     return funcao                   # 'return' seguido da variavel, retorna o valor "real" do argumento inserido na variavel.
-#                                     # tudo q vem abaixo de return, não é levado em conta, return marca o fim.
+def valor(funcao):
+    return funcao                   # 'return' seguido da variavel, retorna o valor "real" do argumento inserido na variavel.
+                                     # tudo q vem abaixo de return, não é levado em conta, return marca o fim.
 
-# var = valor('valor que eu quero')   # o valor 'none' retornado na função quando NÃO utilizado 'return' indica que: 
-#                                      # o dado/argumento apresentado não possui nenhum tipo de valor
-# if var:
-#     print(var)
-# else:
-#     print('nenhum valor')
-
-## -----------------------------------------------------------
-
-# def divisao(n1, n2):
-#     if n2 == 0:       # divisões por zero resultam em erro, para isso, uma solução é checar antes, desse modo, é possivel inserir mais returns na mesma função
-#         return
-#     return n1 / n2    # pode-se tambem retornar a variavel executando alguma ação
-
-# divide = divisao(10, 10)
-# if divide:
-#     print(divide)
-# else:
-#     print('conta inválida')
+var = valor('valor que eu quero')   # o valor 'none' retornado na função quando NÃO utilizado 'return' indica que: 
+                                      # o dado/argumento apresentado não possui nenhum tipo de valor
+if var:
+    print(var)
+else:
+    print('nenhum valor')
 
 # -----------------------------------------------------------
 
-# def funcao():
-#     return [1, 'um', True, 1.1]
-# var = funcao()
-# print(var, type(var))   # pode-se retornar valores de qualquer tipo
+def divisao(n1, n2):
+    if n2 == 0:       # divisões por zero resultam em erro, para isso, uma solução é checar antes, desse modo, é possivel inserir mais returns na mesma função
+        return
+    return n1 / n2    # pode-se tambem retornar a variavel executando alguma ação
+
+divide = divisao(10, 10)
+if divide:
+    print(divide)
+else:
+    print('conta inválida')
 
 # -----------------------------------------------------------
 
-'''WORK AROUND'''
+def funcao():
+    return [1, 'um', True, 1.1]
+var = funcao()
+print(var, type(var))   # pode-se retornar valores de qualquer tipo
 
-# def value(amaount):
-#     print(amaount)
+# -----------------------------------------------------------
 
-# def value2():
-#     return value
+'''JURY-RIG'''
 
-# var = value2()
+def value(amaount):
+    print(amaount)
 
-# print(type(var))
+def value2():
+    return value
+
+var = value2()
+
+print(type(var))
 
 # --------------------------------------------------------
