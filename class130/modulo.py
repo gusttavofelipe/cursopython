267b0ab42ce913f3a66a002cf9336a31e8e379ad
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-08-14 20:45:36 -0300

    if __name__ == __main__

---
+++ class130/modulo.py
@@ -0,0 +1,11 @@
+
+def soma(x: int or float, y: int or float):
+    return x + y
+
+
+if __name__ == '__main__':
+    print(soma(10, 10))
+    print(soma(6, 30))
+
+# a checagem define que o código dentro dela
+# será executado apenas no arquivo principal (__main__) 
